19bce1db60a6f4d5695786fe997addc7bd76497d
himanshukumar76 <himanshukumar75655893@gmail.com>
2024-04-13 16:27:52 +0530

    01_speak.py and text.py files added

---
+++ 01_speak.py
@@ -0,0 +1,22 @@
+# for voice
+import pyttsx3
+
+eg = pyttsx3.init()
+voices = eg.getProperty("voices")
+# print(voices[0].id)
+# print(voices[1].id)
+newVoiceRate = 130
+eg.setProperty('rate', newVoiceRate)
+
+
+# for voice
+def speak(audio):
+    eg.say(audio)
+    eg.runAndWait()
+
+
+# our own files(make it to read automatically using speech recognization system )
+if __name__ == "__main__":
+    with open("text.txt", "r") as f:
+        value = f.read()
+        speak(value)
