94af08d226cdcea62d414a96019e2231ea8929c1
himanshukumar76 <himanshukumar75655893@gmail.com>
2024-04-23 17:19:06 +0530

    Some new file added and some new files modified

--- JARVIS/JAVIS.py
+++ JARVIS/JAVIS.py
@@ -1,11 +1,12 @@
 import pyttsx3
 import speech_recognition as sr
 import datetime
-
+import wikipedia
+import webbrowser
 
 eg = pyttsx3.init()
 voices = eg.getProperty("voices")
-newVoiceRate = 180
+newVoiceRate = 190
 eg.setProperty('rate', newVoiceRate)
 eg.setProperty('voice', voices[0].id)
 
@@ -19,35 +20,71 @@ def speak(audio):
 def wishMe():
     hour = int(datetime.datetime.now().hour)
     if hour >= 0 and hour < 12:
-        speak("Good Morning , sir!")
+        speak("Good Morning !")
     elif hour >= 12 and hour < 18:
-        speak("Good after noon, sir!")
+        speak("Good after noon !")
     else:
         speak("Good evening, sir!")
 
-    speak("I'm Jarvis. Please! tell me , how may I help you? ")
+    speak("I'm Jarvis. Please!  how may I help you? ")
 
-# to take input from the user 
+# to take input from the user
+
+
 def takeInput():
     ''' It takes microphone input from the user and return string output. '''
     r = sr.Recognizer()
 
-    try: 
+    with sr.Microphone() as source:
         print("Listenning .........")
-        with sr.Microphone() as source:
-            
-            r.adjust_for_ambient_noise(source,duration=0.2)
+        r.adjust_for_ambient_noise(source, duration=0.2)
+        audio = r.listen(source)
 
-            audio = r.listen(source)
+    try:
 
-            text = r.recognize_google(audio)
-            print("Did you say : ",text)
-            speak(text)
+        print("Recognizing ... ")
+        query = r.recognize_google(audio)
+        print(f"User said: {query}")
+        # speak(text)
 
-    except: 
+    except Exception as e:
         print("Sorry! Please repeat again .... ")
+        return "None"
+
+    return query
+
 
 # our own files(make it to read automatically using speech recognization system )
 if __name__ == "__main__":
     wishMe()
-    takeInput()
+    while True:
+        query = takeInput().lower()
+
+        # logic to executing tasked based on query
+        if 'wikipedia' in query:
+            speak("Searching wikipedia ...... ")
+            query = query.replace('wikipedia', '')
+            results = wikipedia.summary(query, sentences=3)
+            speak("According to wikipedia.... ")
+            print()
+            print(results)
+            speak(results)
+
+        elif 'youtube' in query:
+            webbrowser.open("www.youtube.com")
+            
+        elif 'google' in query: 
+            webbrowser.open("www.google.com")
+            
+        elif 'facebook' in query:
+            webbrowser.open("www.facebook.com")
+            
+        elif 'geeks' in query:
+            webbrowser.open("https://www.geeksforgeeks.org/user/himanshukumnq5k/")
+
+        elif 'close' in query:
+            print()
+            print()
+            print("Thanks for using... ")
+            speak("Thanks for using...")
+            break
--- SNAKE GAME/01_DAY1.py
+++ SNAKE GAME/01_DAY1.py
@@ -1,34 +1,78 @@
 # SNAKE GAME
-
 import pygame
 from pygame.locals import *
 import os
 
 
-def game():
-    pass
+class Game:
+    def __init__(self):
+        # initialize the pygame module
+        pygame.init()
+
+        self.w = 1300
+        self.h = 670
+        self.surface = pygame.display.set_mode((self.w,self.h))
+
+        self.snake = snake(self.surface)
+        self.snake.draw()
+
+    def run(self):
+
+        running = True
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+
+                    if event.key == K_UP:
+                        self.snake.move_up()
+
+                    if event.key == K_DOWN:
+                        self.snake.move_down()
+
+                    if event.key == K_LEFT:
+                        self.snake.move_left()
+
+                    if event.key == K_RIGHT:
+                        self.snake.move_right()
+
+                elif event.type == QUIT:
+                    running = False
+
+
+class snake:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.img_blc = pygame.image.load("resources/block.jpg")
+        self.img_blc.convert()
+        self.x = 100
+        self.y = 100
+
+    def draw(self):
+        self.parent_screen.fill((110, 247, 110))
+        self.parent_screen.blit(self.img_blc, (self.x, self.y))
+        pygame.display.flip()
+
+    def move_up(self):
+        self.y = self.y - 10
+        self.draw()
+
+    def move_down(self):
+        self.y = self.y + 10
+        self.draw()
+
+    def move_left(self):
+        self.x = self.x - 10
+        self.draw()
+
+    def move_right(self):
+        self.x = self.x + 10
+        self.draw()
 
 
 if __name__ == "__main__":
-    # initialize the pygame module
-    pygame.init()
 
-    # initialize the pygame display module
-    pygame.display.init()
-
-    surface = pygame.display.set_mode((1200, 650))
-    surface.fill((110, 247, 110))
-    pygame.display.flip()
-
-    # for the block
-    block = pygame.image.load("block.jpg").convert()
-    surface.blit(block, (0, 0))
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    running = False
-            elif event.type == QUIT:
-                running = False
+    game = Game()
+    game.run()
